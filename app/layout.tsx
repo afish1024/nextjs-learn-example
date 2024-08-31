ea67eb20b634f2ffe14f13c3a4d18125f77d7f74
yantp <yantp888@icloud.com>
2024-08-31 18:34:54 +0800

    Adding Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
+import { Metadata } from 'next';
 
 // 添加全局 CSS 文件
 import './ui/global.css'
 
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  // 模板中的 %s 将替换为特定的页面标题。
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
